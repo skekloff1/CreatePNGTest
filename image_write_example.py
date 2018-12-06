74217576d533bf898f11c16eaba507413530221b
Goro <goro@iMac.local>
2018-12-06 08:13:43 -0500

    base commit

---
+++ image_write_example.py
@@ -0,0 +1,54 @@
+"""
+Sample code to show how to create an image by
+coloring in each pixel manually!  This is by making a
+formula to generate a color value base on each pixel position
+
+This uses Python Image Library (PIL/pillow)
+
+Ref
+https://en.wikibooks.org/wiki/Python_Imaging_Library/Editing_Pixels
+"""
+import os
+from PIL import Image
+
+# init the image size (note: they don't need to be the same)
+# TODO: try changing these
+IMG_X_SIZE = 512
+IMG_Y_SIZE = 512
+IMG_SIZE_TUPLE = (IMG_X_SIZE, IMG_Y_SIZE)
+
+def pick_pixel_color(x,y):
+    red_intensity = 0
+    green_intensity = 128
+    # TODO: try setting the green intensity to one of these (and try your own)
+    # green_intensity = 255 if y > 255 else y
+    # green_intensity = y % 256
+    # green_intensity = y % 256 + x % 256
+    # green_intensity = x*y % 256
+    blue_intensity =0
+    color = (red_intensity, green_intensity, blue_intensity)
+
+    return color
+
+# Set the dir and filename stuff
+my_dir = os.path.dirname(os.path.realpath(__file__))
+ofname = 'output_image.png'
+full_ofname = my_dir + '/' + ofname
+
+# Initialize the image as an RGB (24bit color) image
+# and init the pixel from that image
+my_image = Image.new('RGB', IMG_SIZE_TUPLE)
+my_pixels = my_image.load()
+
+# Get the image dimension so we can iterate over it
+image_x_size = my_image.size[0]
+image_y_size = my_image.size[1]
+
+# iterate over all the pixels
+for y in range(image_y_size):
+    for x in range(image_x_size):
+        pixel_color = pick_pixel_color(x,y)
+        my_pixels[x, y] = pixel_color
+
+# save the image as a PNG file
+my_image.save(full_ofname, 'png')
